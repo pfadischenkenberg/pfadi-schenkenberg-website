52e2b305cc3e288453daea78d83f3c0b2b941b7f
timlandolt <timlandolt@gmx.ch>
2024-08-03 18:03:08 +0200

    Hello Vue! + favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+    app: {
+        head: {
+            link: [
+                {rel: 'icon', type: 'image/svg+xml', href: '/schenkenberg-wappen.svg'},
+                {rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png'},
+                {rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png'},
+            ]
+        }
+    },
+    compatibilityDate: '2024-04-03',
+    devtools: {enabled: true},
+
+
 })
