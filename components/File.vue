139a437e5de2da8e0bb1c9e75e3bdf517e6603bb
Tim Landolt <timlandolt@gmx.ch>
2024-08-10 10:41:17 +0200

    Added file icon, used new instagram logo

--- components/File.vue
+++ components/File.vue
@@ -17,9 +17,18 @@ const fileName = props.src.split(/[/;\\]+/).pop();
 </script>
 
 <style lang="scss" scoped>
+a {
+  display: flex;
+  align-items: center;
+}
 a::before {
   content: "";
+  width: 2rem;
+  height: 2rem;
   display: inline-block;
-
+  background-image: url("assets/img/file-icon.svg");
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
 }
 </style>
