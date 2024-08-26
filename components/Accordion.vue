6043a531780685bf919d38d1a4cdc68c5e4adb13
Tim Landolt <timlandolt@gmx.ch>
2024-08-26 09:38:04 +0200

    Add faq page content
    Add and style faq page content in the form of accordions
    Change the "PRB" links to their new routes
    Add arrow to accordion titlle
    Make all links red and underlined

--- components/Accordion.vue
+++ components/Accordion.vue
@@ -2,6 +2,7 @@
   <div class="accordion">
     <button class="accordion-title" @click="togglePanel">
       <h2>{{ title }}</h2>
+      <img :class="{ open: isOpen }" src="assets/img/arrow-extend.svg" alt="Pfeil">
     </button>
     <div ref="content" :style="{ maxHeight: contentHeight }" class="content">
       <slot />
@@ -10,15 +11,12 @@
 </template>
 
 <script setup>
-
-
 const props = defineProps({
   title: {
     type: String,
     required: true
   }
 });
-
 
 const isOpen = ref(false);
 const contentHeight = ref("0px");
@@ -44,29 +42,40 @@ onMounted(() => {
 @import "assets/scss/variables";
 
 .accordion {
+  margin-bottom: 1rem;
+
   overflow: hidden;
 
   .accordion-title {
-    width: fit-content;
-    background-color: transparent;
-    border: none;
     padding: 0;
-    text-align: left;
+    width: 100%;
+    border: none;
+    margin-bottom: 0.5rem;
+
+    display: flex;
+    justify-content: center;
+
+    background-color: transparent;
     cursor: pointer;
     outline: none;
 
     h2 {
-      transition: color 0.2s ease-out;
+      display: inline-block;
+    }
 
-      &:hover {
-        color: $burnt-sienna-400;
+    img {
+      transition: transform 0.2s ease-in-out;
+      margin: 0 0 .4rem 1rem;
+
+      &.open {
+        transform: rotate(180deg);
       }
     }
   }
 
   .content {
     overflow: hidden;
-    transition: max-height 0.2s ease-out;
+    transition: max-height 0.2s ease-in-out;
   }
 }
 </style>
--- pages/faq.vue
+++ pages/faq.vue
@@ -2,17 +2,56 @@
   <div>
     <DefaultHero>Mögliche Fragen zur</DefaultHero>
     <main class="grid-container">
-      <Accordion title="HElooo0">
-        Hello world<br>
-        Hello world<br>
-        Hello world<br>
-        Hello world<br>
-        Hello world<br>
-        Hello world<br>
-        Hello world<br>
-        Hello world<br>
-        Hello world<br>
-        Hello world<br>
+      <h1>Fragen, die auftauchen könnten</h1>
+      <Accordion title="Was ist Pfadi?">
+        <p>Die Pfadibewegung ist aktuell die grösste Jugendorganisation und steht für Offenheit, Freundschaft,
+          Engagement, Zusammenhalt und Toleranz. Die Pfadi Schenkenberg gehört zu dem Bezirk Zytröseli, welcher aus den
+          5 Pfadi Abteilungen Ramstein, Falkenstein, Schalberg, Bischofstein und Schenkenberg besteht. Der Bezirk gehört
+          zum Kantonalverband "Pfadi Region Basel" welcher Wiederum zur Pfadibewegung Schweiz gehört. Die Pfadi ist eine
+          Freizeitbeschäftigung, bei der das Erleben von Abenteuer in der Natur im Vordergrund steht. Sie bietet die
+          Möglichkeit, voneinander zu lernen und sich gegenseitig zu akzeptieren, und sorgt ebenfalls für Verständnis
+          aller Mitmenschen. Das ausgebildete Leitungsteam will mit dem vielseitigen Programm alle Kinder und
+          Jugendliche ansprechen.</p>
+      </Accordion>
+      <Accordion title="Wann muss der Jahresbeitrag bezahlt werden?">
+        <p> Eine Rechnung für den Jahresbeitrag wird jeweils im Zeitraum nach den Sommerferien für das angebrochene Jahr
+          versendet. Dieser muss bezahlt werden, wenn man zu diesem Zeitpunkt angemeldet ist oder bis vor
+          kurzem angemeldet war.</p>
+      </Accordion>
+      <Accordion title="Welche Ausrüstung braucht man für die Pfadi?">
+        <p>
+          Es gibt ein paar Dinge, die im besten Fall an jedem Anlass dabei sind.<br />
+          <b>ZZ</b> - Zündhölzli und Zeitung, um Feuer zu machen<br />
+          <b>Gueti Jagt / Technix</b> - Diese praktischen Büchlein sind nützlich zum Spuren entziffern, Karte lesen,
+          Knotenmachen und vieles Weiteres, das man in der Pfadi gebrauchen kann. Erhältlich sind diese im
+          <NuxtLink to="https://pfadi-region-basel.ch/projekte/pfadilaedeli/" target="_blank">Pfadilädeli</NuxtLink>
+          und auf
+          <NuxtLink to="https://www.hajk.ch/de/" target="_blank">hajk.ch</NuxtLink>
+          . (Gueti Jagt nur für Wölfli)<br />
+          <b>Sackmesser</b> - Egal, ob zum Schnitzen oder Schnur schneiden, ein Sackmesser kann in so manchen
+          Situationen nützlich sein.<br />
+          <b>Pfadikrawatte</b> - Unsere ikonische orange Pfadikrawatte ist unser Wiedererkennungswert. Sie kann
+          ausserdem vielseitig eingesetzt werden.<br />
+        </p>
+        <p>
+          Weiteres, das vor allem in den Lagern wichtig ist, ist Folgendes:<br />
+          <b>Pfadihemd</b> - Auf dem Pfadihemd sammelt man Drücke und Abzeichen aus den Lagern.<br />
+          <b>Pfadipulli</b> - Wir haben auch einen eigenen Pfadipulli. Diesen kann man ausschliesslich im
+          <NuxtLink to="https://pfadi-region-basel.ch/projekte/pfadilaedeli/" target="_blank">Pfadilädeli</NuxtLink>
+          kaufen.<br />
+          <b>Schlafsack</b> - In den Lagern ist ein Schlafsack natürlich unausweichlich. Welcher der richtige ist,
+          erfährst du
+          <NuxtLink to="https://walking-wild.net/de/wandern-camping-outdoor/campingausrüstung-schlafsack-ratgeber"
+                    target="_blank">hier
+          </NuxtLink>
+          .<br />
+          <b>«Mätteli»</b> - Die Schlafmatte braucht man im AufLa / PfiLa und im SoLa. Auf was du beim Kauf eines
+          «Mätteli» achten musst, kannst du
+          <NuxtLink to="https://www.doorout.com/blog/ratgeber-camping-matten-welche-wann-wo-warum/" target="_blank">
+            hier
+          </NuxtLink>
+          nachlesen.
+        </p>
       </Accordion>
     </main>
   </div>
@@ -20,13 +59,21 @@
 
 <script setup lang="ts">
 
-import Accordion from "~/components/Accordion.vue";
 </script>
 
 <style lang="scss" scoped>
 main {
-  .accordion {
+  h1 {
     grid-column: 1/13;
+    text-align: center;
+  }
+
+  .accordion {
+    grid-column: 2/12;
+
+    p {
+      max-width: 80rem;
+    }
   }
 }
 </style>
