3db2927cf965ac0357b28c60847f494891e83f8e
Tim Landolt <timlandolt@gmx.ch>
2024-10-08 15:26:41 +0200

    Add custom Text for "Pfadistufe"

--- pages/stufen.vue
+++ pages/stufen.vue
@@ -81,12 +81,11 @@ Wir freuen uns auf dich!`
       src: "/kalender/test-kalender.pdf"
     },
     text: {
-      intro: "Bist du zwischen 6 und 11 Jahre alt und willst die Welt der Wölfli sowie viele verschiedene Abenteuer erleben? Dann bist du bei uns in der Wölfli-Stufe der Pfadi Schenkenberg genau Richtig!",
-      main: `Bei uns geht es darum, Spiele zu spielen, etwas Neues zu lernen, Spass zu haben und zusammen spannende Abenteuer zu erleben. Dabei versuchen wir in dem Pfingstlager, Herbstlager oder an den Samstagnachmittagen verschiedenen Gestalten zu helfen. Wobei wir einander nie im Stich lassen!
-Am Samstag treffen wir uns entweder an den Orten direkt (hier geht es zum Quartalplan). Wir bieten oft auch einen Bring-und-hol-Dienst an, bei dem wir uns am Aeschenplatz treffen und von dort aus in den Wald um Basel gehen.<br/><br/>
-Wir geben immer alles, um die beste Zeit zusammen zu verbringen und würden uns sehr freuen, neue Gesichter begrüssen zu dürfen.<br/><br/>
-Wenn du Lust hast, schnuppern zu kommen, kannst du einfach an einem Samstagnachmittag vorbeischauen!<br/><br/>
-Wir freuen uns auf dich!`
+      intro: "Du bist zwischen 11 und 14 Jahren alt? Dann ist die Pfadistufe die richtige Stufe für dich. Gemeinsam verbringen wir Samstagnachmittage mit sportlichen Spielen, kreativen Aufgaben und natürlich viel Spass!",
+      main: `In der Pfadistufe begegnen wir verschiedensten Wesen, mit denen wir Spielen, Rätsel lösen & mit unseren kreativen Ideen helfen. Wir verbringen die meisten Samstage draussen im Wald, wo wir uns sportlich austoben, die Natur kennenlernen und als Gruppe zusammenwachsen. Aber auch Aktivitäten wie gemeinsames Kochen, einen Ausflug in die Kletterhalle, oder ein Filmabend dürfen nicht fehlen. Das Highlight des Pfadijahres ist das Sommerlager, in welchem wir zwei Wochen draussen & im Zelt verbringen. Nebst vielem Spielen kommt das Wandern, Erlernen nützlicher Pfaditechnik und Singen am Lagerfeuer natürlich nicht zu kurz!
+<br/><br/>
+Komm gerne einfach vorbei, wir freuen uns auf dich!
+`
     }
   },
   {
