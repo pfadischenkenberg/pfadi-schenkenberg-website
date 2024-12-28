3848e1003bfe327eba77cea200258cc287e5a263
Tim Landolt <timlandolt@gmx.ch>
2024-12-28 19:37:59 +0100

    Make accordion more accessible

--- components/Accordion.vue
+++ components/Accordion.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="accordion">
-    <button class="accordion-title" @click="togglePanel">
+    <button class="accordion-title" @click="toggleAccordion" :title="title">
       <h2>{{ title }}</h2>
       <img :class="{ open: isOpen }" src="assets/img/arrow-extend.svg" alt="Pfeil">
     </button>
     <div ref="content" :style="{ maxHeight: contentHeight }" class="content">
-      <slot />
+      <slot v-if="isOpen" />
     </div>
   </div>
 </template>
@@ -15,34 +15,44 @@ const props = defineProps({
   title: {
     type: String,
     required: true
+  },
+  isOpen: {
+    type: Boolean,
+    required: false,
+    default: false
   }
 });
 
-const isOpen = ref(false);
-const contentHeight = ref("0px");
+const isOpen = ref(props.isOpen);
+const contentHeight = ref("0px"); // css max-height of content
 const content = ref(null);
 
-const togglePanel = () => {
+
+onMounted(adjustContentHeight);
+
+function toggleAccordion() {
   isOpen.value = !isOpen.value;
   adjustContentHeight();
-};
+}
 
-const adjustContentHeight = () => {
+// Sets the max-height attribute of the content to the respective height
+function adjustContentHeight() {
   nextTick(() => {
-    contentHeight.value = isOpen.value ? `${content.value.scrollHeight}px` : "0px";
+    if (isOpen.value) {
+      contentHeight.value = `${content.value.scrollHeight}px`;
+    } else {
+      contentHeight.value = "0px";
+    }
   });
-};
+}
 
-onMounted(() => {
-  contentHeight.value = isOpen.value ? `${content.value.scrollHeight}px` : "0px";
-});
 </script>
 
 <style lang="scss" scoped>
 @use "assets/scss/variables";
 
 .accordion {
-  margin-bottom: 1rem;
+  margin-bottom: 2rem;
 
   overflow: hidden;
 
@@ -53,11 +63,12 @@ onMounted(() => {
     margin-bottom: 0.5rem;
 
     display: flex;
-    justify-content: center;
+    align-items: center;
+
+    text-align: left;
 
     background-color: transparent;
     cursor: pointer;
-    outline: none;
 
     h2 {
       display: inline-block;
