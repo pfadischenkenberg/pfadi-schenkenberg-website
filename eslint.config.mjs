c0b7e83dc9fbd537bd8418881362d6854bf6fb49
Tim Landolt <timlandolt@gmx.ch>
2026-01-11 21:45:04 +0100

    Update eslint config
    to enable autofix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,30 @@
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt(
+  // JS / TS (NOT Vue)
   {
+    files: ["**/*.{js,ts}"],
     rules: {
       quotes: ["error", "double", { avoidEscape: true }],
-      "no-console": ["error"],
+      "no-console": "error",
       "comma-dangle": ["error", "always-multiline"],
       indent: ["error", 2, { SwitchCase: 1 }],
-      semi: ["error", "always"],
+      semi: ["error", "always"]
+    }
+  },
 
+  // Vue SFCs
+  {
+    files: ["**/*.vue"],
+    rules: {
+      quotes: ["error", "double", { avoidEscape: true }],
+      "no-console": "error",
+      "comma-dangle": ["error", "always-multiline"],
+      indent: "off",
       "vue/html-indent": ["error", 2],
-      "vue/script-indent": ["error", 2, { baseIndent: 1 }]
+      "vue/script-indent": ["error", 2, { baseIndent: 1 }],
+      semi: ["error", "always"],
+      "vue/multi-word-component-names" : "off",
     }
   }
 );
