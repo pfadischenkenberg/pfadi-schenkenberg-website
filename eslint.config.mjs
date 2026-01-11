911c33217b7ec9c250e073e580e8911bc4a2ab1b
Tim Landolt <timlandolt@gmx.ch>
2026-01-11 21:14:49 +0100

    Configure eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,16 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt(
-  // Your custom configs here
-)
+  {
+    rules: {
+      quotes: ["error", "double", { avoidEscape: true }],
+      "no-console": ["error"],
+      "comma-dangle": ["error", "always-multiline"],
+      indent: ["error", 2, { SwitchCase: 1 }],
+      semi: ["error", "always"],
+
+      "vue/html-indent": ["error", 2],
+      "vue/script-indent": ["error", 2, { baseIndent: 1 }]
+    }
+  }
+);
